7dc87905aa1abb2a452422fe25342808173bcf14
AzizKlai <mohamedaziz.klai@insat.u-carthage.tn>
2024-01-31 23:25:12 +0100

    dashboard home

--- src/app/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { BlogPostsService } from 'src/app/services/blog-posts.service';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { CommentsService } from 'src/app/services/comments.service';
+import { SubscribersService } from 'src/app/services/subscribers.service';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -7,4 +12,24 @@ import { Component } from '@angular/core';
 })
 export class DashboardHomeComponent {
 
+  postsCount$: Observable<number>;
+  commentsCount$: Observable<number>;
+  categoriesCount$: Observable<number>;
+  subCount$: Observable<number>;
+
+  postsService  = inject(BlogPostsService)
+  commentsService = inject(CommentsService)
+  categoriesService = inject(CategoriesService)
+  subService = inject(SubscribersService)
+
+  constructor() {
+    this.postsCount$ = this.postsService.loadData().pipe(map( data=>data.length ));
+    this.commentsCount$ = this.commentsService.loadAllData().pipe(map( data=>data.length ));
+    this.categoriesCount$ = this.categoriesService.loadData().pipe(map( data=>data.length ));
+    this.subCount$ = this.subService.loadData().pipe(map( data=>data.length ));
+    
+  }
+
 }
+
+
